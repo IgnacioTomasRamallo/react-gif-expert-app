07b7491709404ad6e76c4c26a68c3008d9b9e011
Ignacio Ramallo <ignacioramalloherrera@gmail.com>
2023-01-19 17:20:29 -0300

    primer comit

---
+++ src/GiftExpertApp.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react"
+import { AddCategory, GifGrid } from "./components"
+
+
+export const GiftExpertApp = () => {
+    
+    const [categories, setCategories] = useState([])
+
+    const onAddCategory = (newCategory) =>{
+        if( categories.includes(newCategory ) )return;
+        
+        // categories.push(newCategory)
+        setCategories([ newCategory , ...categories]);
+        // setCategories(['Valorant', ...categories])
+    }
+    return (
+        <>
+             {/*Titulo App  */}
+            <h1>GiftExpertApp</h1>
+            {/* Input */}
+            <AddCategory 
+                onNewCategory={ event => onAddCategory(event) }
+                
+            />
+
+            {/* Listado de Gif */}
+            
+            <ol>
+                {
+                    categories.map( (category) =>
+                        <GifGrid 
+                            key={ category } 
+                            category={ category }
+                        />
+                        )
+            } 
+            
+
+            </ol>
+
+        </>
+    )
+}
